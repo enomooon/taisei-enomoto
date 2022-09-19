b8046199610a6836882a3a028ad9dfb19a5751c0
enomooon <enomoto-taisei@ed.tmu.ac.jp>
2022-09-19 19:14:41 +0900

    change char size

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-var window_width = document.documentElement.clientWidth
-var window_height = document.documentElement.clientHeight
+let window_width = document.documentElement.clientWidth
+let window_height = document.documentElement.clientHeight
 
-//参考 https://myscreate.com/dom-acquisition/
+let out_link = document.getElementsByClassName('out-link');
+const out_link_array = Array.prototype.slice.call(out_link);
+
+let headline = document.getElementsByClassName("headline")[0];
+headline.addEventListener('mouseenter', () => {
+    headline.style.backgroundColor = "#C0C0C0";
+}, false); 
+headline.addEventListener('mouseleave', () => {
+    headline.style.backgroundColor = "#FFFFFF";
+}, false);
+
 (window.onload = function() {
-    let out_link = document.getElementsByClassName('out-link');
-    const out_link_array = Array.prototype.slice.call(out_link);
     out_link_array.forEach(function(item) {
         item.style.opacity =  1;
     });
 })();
+
+// let sub_title = document.getElementsByClassName('sub-title');
+// const sub_title_array = Array.prototype.slice.call(sub_title);
+// sub_title_array.forEach(function(item) {
+//     item.addEventListener('mouseenter', () => {
+//         item.style.color = "#909090";
+//     }, false); 
+//     item.addEventListener('mouseleave', () => {
+//         item.style.color = "#000000";
+//     }, false);
+// });
+
